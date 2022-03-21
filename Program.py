8ee166654a31d4e2a1cecf79a060b0d38a90252f
Fadhillahusein <97729005+Fadhillahusein@users.noreply.github.com>
2022-03-21 22:57:34 +0700

    Update Program.py

--- Program.py
+++ Program.py
@@ -1,7 +1,6 @@
 #python drowniness_yawn.py --webcam webcam_index
 
 from scipy.spatial import distance as dist
-from imutils.video import VideoStream
 from imutils import face_utils
 from threading import Thread
 import numpy as np
@@ -60,20 +59,6 @@ def final_ear(shape):
     ear = (leftEAR + rightEAR) / 2.0
     return (ear, leftEye, rightEye)
 
-def lip_distance(shape):
-    top_lip = shape[50:53]
-    top_lip = np.concatenate((top_lip, shape[61:64]))
-
-    low_lip = shape[56:59]
-    low_lip = np.concatenate((low_lip, shape[65:68]))
-
-    top_mean = np.mean(top_lip, axis=0)
-    low_mean = np.mean(low_lip, axis=0)
-
-    distance = abs(top_mean[1] - low_mean[1])
-    return distance
-
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-w", "--webcam", type=int, default=0,
                 help="index of webcam on system")
@@ -81,21 +66,18 @@ args = vars(ap.parse_args())
 
 EYE_AR_THRESH = 0.26
 EYE_AR_CONSEC_FRAMES = 30
-YAWN_THRESH = 20
 alarm_status = False
 alarm_status2 = False
 saying = False
 COUNTER = 0
 
 print("-> Loading the predictor and detector...")
-detector2 = dlib.get_frontal_face_detector()
-detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")    #Faster but less accurate
+detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 
 
-print("-> Starting Video Stream")
+#print("-> Starting Video Stream")
 vs = VideoStream(src=args["webcam"]).start()
-
 #vs= VideoStream(usePiCamera=True).start()       //For Raspberry Pi
 time.sleep(1.0)
 COUNTER1 = 0
@@ -104,10 +86,10 @@ while True:
     frame = imutils.resize(frame, width=450)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector2(gray)
-    #rects = detector(gray, 0)
-    rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
+    rects = detector(gray, 0)
+    '''rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
 		minNeighbors=5, minSize=(30, 30),
-		flags=cv2.CASCADE_SCALE_IMAGE)
+		flags=cv2.CASCADE_SCALE_IMAGE)'''
     cv2.putText(frame, 'Focust Assistant', (10, 40), font, 1, (255, 51, 51), 4)
     #for rect in rects:
     if (len(faces) == 0):
